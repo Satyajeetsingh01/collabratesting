eb0cfc3c21700858a69cfa1058fedd7c622b00d7
Satyajeetsingh01 <satyajeetsingh.official@gmail.com>
2022-01-27 12:22:17 +0530

    SELENIUM ALL METHODS AND ASSIGNMENTS

---
+++ ElfCollabera/src/test/java/contacts/TC_contacts_001.java
@@ -0,0 +1,18 @@
+package contacts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC_contacts_001 {
+	
+	@Test
+	public void demo()
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+	}
+
+}
